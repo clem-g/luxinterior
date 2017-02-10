871931b4609d6a9293de84883815671890953795
clem-g <clement.gaidon@pnnl.gov>
2017-02-10 15:39:03 -0800

    Replaced HTTP server with websockets

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
-#include "Arduino.h"
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
-#include "Adafruit_SSD1306.h"
+#include <WebSocketsServer.h>
+#include <Hash.h>
+#include <Adafruit_SSD1306.h>
 #include <Adafruit_TSL2561_U.h>
 
 // Wifi credentials
@@ -9,6 +12,9 @@ const char* password = "your_pwd";
 
 // Server port settings
 ESP8266WebServer server(80);
+WebSocketsServer webSocket=WebSocketsServer(88);
+String webSite,javaScript,JSONtxt;
+unsigned long websockCount=0;
 
 // Push buttons
 const int buttonAPin =  0;
@@ -25,19 +31,58 @@ uint16_t ir = 0;
 uint32_t lx = 0;
 String gain = "1x";
 String inte = "13.7ms";
-  
+
 // OLED screen
 #define OLED_RESET 16
 Adafruit_SSD1306 display(OLED_RESET);
- 
-void setup(void) {
+
+// JavaScript
+void buildJavascript(){
+  javaScript="<SCRIPT>\n";
+  javaScript+="InitWebSocket();\n";
+  javaScript+="function InitWebSocket(){\n";
+  javaScript+="  websock=new WebSocket('ws://'+window.location.hostname+':88/');\n";
+  javaScript+="  websock.onmessage=function(evt){\n";
+  javaScript+="    JSONobj=JSON.parse(evt.data);\n";
+  javaScript+="    document.getElementById('illuminance').innerHTML=JSONobj.illuminance;\n";
+  javaScript+="    document.getElementById('broadband').innerHTML=JSONobj.broadband;\n";
+  javaScript+="    document.getElementById('infrared').innerHTML=JSONobj.infrared;\n";
+  javaScript+="    document.getElementById('gain').innerHTML=JSONobj.gain;\n";
+  javaScript+="    document.getElementById('inte').innerHTML=JSONobj.inte;\n";
+  javaScript+="  }\n";
+  javaScript+="}\n";
+  javaScript+="</SCRIPT>\n";
+}
+
+// HTML
+void buildWebsite(){
+  buildJavascript();
+  webSite="<!DOCTYPE HTML><HTML>\n";
+  webSite+="<META name='viewport' content='width=device-width, initial-scale=1'>\n";
+  webSite+=javaScript;
+  webSite+="<BODY>\n";
+  webSite+="<A ID='illuminance'></A>lux<br>\n";
+  webSite+="(<A ID='broadband'></A>;<A ID='infrared'></A>)<br><br>\n";
+  webSite+="<A ID='gain'></A> / <A ID='inte'></A>\n";
+  webSite+="</BODY>\n";
+  webSite+="</HTML>\n";
+}
+
+// Webpage
+void handleWebsite(){
+  buildWebsite();
+  server.send(200,"text/html",webSite);
+}
+
+
+void setup() {
 
   // Push button
   pinMode(buttonAPin, INPUT_PULLUP);
   pinMode(buttonCPin, INPUT_PULLUP);
   attachInterrupt(buttonAPin, buttonA_ISR, FALLING);
   attachInterrupt(buttonCPin, buttonC_ISR, FALLING);
-  
+
   // OLED screen
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
   display.clearDisplay();
@@ -49,7 +94,7 @@ void setup(void) {
   tsl.enableAutoRange(false); // No auto-gain for this application
   tsl.setGain(TSL2561_GAIN_1X); // 1X for bright conditions and 16X to boost sensitivity for dim conditions (! saturated sensor will clip and output 0)
   tsl.setIntegrationTime(TSL2561_INTEGRATIONTIME_13MS); // tradeoff with the sigma-delta ADC resolution 13MS (13.7ms) / 101MS / 402MS (full 16bit)
-  
+
   // Connect to WiFi network
   display.print("Connecting to ");
   display.print(ssid);
@@ -67,28 +112,27 @@ void setup(void) {
       break;
     }
   }
-  
+
+  // If not, just display
   if(notConnected) {
     display.print("\nNetwork not found.");
   } else {
     display.print("\nWiFi connected at:\n");
     display.print(WiFi.localIP());
-    
-    server.on("/", [](){   
-      server.send(200, "text/plain", "{\n  'illuminance':'"+String(lx)+"',\n  'raw_broadband':'"+String(bb)+"',\n  'raw_infrared':'"+String(ir)+"'\n}");
-    });
-    
+    WiFi.mode(WIFI_STA);
+    server.on("/",handleWebsite);
     server.begin();
-
+    webSocket.begin();
   }
 
   display.display();
   delay(4000);
   display.clearDisplay();
-  
+
 }
- 
-void loop(void) {
+
+
+void loop() {
 
   // Test if the deep sleep mode interrupt fired, wakeup using reset
   if(deepsleepFlag) {
@@ -127,10 +171,19 @@ void loop(void) {
     }
   }
   
-  server.handleClient();
-  
   tsl.getLuminosity(&bb,&ir);
   lx = tsl.calculateLux(bb,ir);
+
+  webSocket.loop();
+  server.handleClient();
+
+  websockCount++;
+  JSONtxt="{\"illuminance\":\""+(String)lx+"\","+
+           "\"broadband\":\""+(String)bb+"\","+
+           "\"infrared\":\""+(String)ir+"\","+
+           "\"gain\":\""+gain+"\","+
+           "\"inte\":\""+inte+"\"}";
+  webSocket.broadcastTXT(JSONtxt);
 
   display.clearDisplay();
   display.setCursor(0,0);
@@ -139,4 +192,4 @@ void loop(void) {
   display.print("\n\n"); display.print(gain); display.print(" / ");display.print(inte);
   display.display();
 
-} 
+}
